432d4e9c81fb4333ff29a5cf1890f0a738728cbc
Mabeliris <mabellecfernandez@gmail.com>
2026-02-01 20:13:07 -0300

    first commit

---
+++ src/main/java/com/aluracursos/registroanimales/RegistroanimalesApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.registroanimales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroanimalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroanimalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/registroanimales/RegistroanimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.registroanimales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroanimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
